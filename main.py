e0642845463d9c9f0de2e503518bbe0c38e1b3b0
rushyendra28 <rushyendragusidi@gmail.com>
2025-07-08 05:56:16 +0530

    tada

--- main.py
+++ main.py
@@ -9,14 +9,95 @@
 
 
 # swapping values of variables
-a = input("Enter the value of a: ")
-b = input("Enter the value of b: ")
+# a = input("Enter the value of a: ")
+# b = input("Enter the value of b: ")
 
-print(f"Original values of a and b are: {a} and {b}")
+# print(f"Original values of a and b are: {a} and {b}")
 
-temp = a
-a = b
-b = temp
+# temp = a
+# a = b
+# b = temp
 
-print(f"Swapped values of a and b are: {a} and {b}")
+# print(f"Swapped values of a and b are: {a} and {b}")
 
+
+# Generate a random number between 1 and 1000
+# import random
+# print(f"Random number between 1 and 1000: {random.randint(1, 1000)}")
+
+
+
+#km to miles conversion
+
+# km = float(input("Entwe the distance in Kilometers: "))
+# con_fac = 0.621371
+# miles = km * con_fac
+# print(f"The distance in miles is: {miles}")
+
+
+
+#celcius to fahrenheit conversion
+
+# c = float(input("Enter the temperature in celcius: "))
+# f = c* 9/5 + 32
+# print(f"The temperature in fahrenheit is: {f}")
+
+
+
+#display calender
+
+# import calendar
+
+# year = int(input("Enter the year: "))
+# month = int(input("Enter the month: "))
+# cal = calendar.month(year, month)
+
+# print(cal)
+
+
+
+#solve quadratic equation
+
+# import math
+
+# a = float(input("Enter the coefficient a: "))
+# b = float(input("Enter the coefficient b: "))
+# c = float(input("Enter the coefficient c: "))
+
+# discriminant = b**2 - 4*a*c
+
+# if discriminant > 0:
+#     root1 = (-b + math.sqrt(discriminant)) / (2*a)
+#     root2 = (-b - math.sqrt(discriminant)) / (2*a)
+#     print(f"Root 1: {root1}")
+#     print(f"Root 2: {root2}")
+
+# elif discriminant == 0:
+#     root = -b / (2*a)
+#     print(f"Root: {root}")
+
+# else:
+#     realPart = -b / (2*a)
+#     imaginaryPart = math.sqrt(-discriminant) / (2*a)
+#     print(f"Root 1: {realPart} + {imaginaryPart}i")
+#     print(f"Root 2: {realPart} - {imaginaryPart}i")
+
+
+#swapping without temp
+
+# a = 69
+# b = 420
+# print(f"Before swaping: {a} and {b}")
+# a, b = b, a
+# print(f"Swapped values of a and b are: {a} and {b}")
+
+
+#check if the given number is positive, negative or zero
+# num = int(input("Enter a number: "))
+
+# if num > 0:
+#     print(f"{num} is positive")
+# elif num == 0:
+#     print("Zero")
+# else:
+#     print(f"{num} is negative")
