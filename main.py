0d6cff6160b54b1870bf19b9f144db1814fbeee6
rushyendra28 <rushyendragusidi@gmail.com>
2025-07-08 14:29:31 +0530

    update

--- main.py
+++ main.py
@@ -101,3 +101,37 @@
 #     print("Zero")
 # else:
 #     print(f"{num} is negative")
+
+
+#check if numeber is even or odd
+# num = int(input("Enter a number:"))
+
+# if num % 2 == 0:
+#     print(f"{num} is even")
+# else:
+#     print(f"{num} is odd")
+
+
+#check for leap year
+# year = int(input("Enter a year: "))
+
+# if (year % 4 == 0 and year % 100 != 0) or (year % 400 == 0):
+#     print(f"{year} is a leap year")
+# else:
+#     print(f"{year} is not a leap year")
+
+
+# check for prime number
+# num = int(input("Enter a number: "))
+# is_prime = True
+# for i in range(2, int(num**0.5) + 1):
+#     if num % i == 0:
+#         is_prime = False
+#         break
+# if is_prime and num > 1:
+#     print(f"{num} is a prime number")
+# else:
+#     print(f"{num} is not a prime number")
+
+
+#
