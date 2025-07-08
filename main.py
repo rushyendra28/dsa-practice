8d33fd70ca2ec4878015eb399a386467bbfb721d
rushyendra28 <rushyendragusidi@gmail.com>
2025-07-08 05:30:06 +0530

    practice

--- main.py
+++ main.py
@@ -1,1 +1,22 @@
-print("Need to change")
+# find area of a trainagle
+
+# base = float(input("Enter the Base length of the traingle: "))
+# height = float(input("Enter the height length of the traingle: "))
+
+# area = 0.5 * base * height
+
+# print(f"The area of the traingle is: {area}")
+
+
+# swapping values of variables
+a = input("Enter the value of a: ")
+b = input("Enter the value of b: ")
+
+print(f"Original values of a and b are: {a} and {b}")
+
+temp = a
+a = b
+b = temp
+
+print(f"Swapped values of a and b are: {a} and {b}")
+
